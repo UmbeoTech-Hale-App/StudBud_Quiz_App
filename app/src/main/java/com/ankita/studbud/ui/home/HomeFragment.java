cfc7e85fca37f9d8f83081c9e5f64f6b84e53eb7
aagam109 <aagamshah1090@gmail.com>
2019-10-05 17:38:46 +0530

    Updated HomeDashBoard

--- app/src/main/java/com/ankita/studbud/ui/home/HomeFragment.java
+++ app/src/main/java/com/ankita/studbud/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.ankita.studbud.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,23 +15,39 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.ankita.studbud.R;
+import com.ankita.studbud.Register_Student_Activity;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+
+    ImageView aiMl;
+    ImageView android;
+    ImageView progLang;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+
         return root;
     }
+
+   /* @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //get the button view
+        aiMl = (ImageView) getView().findViewById(R.id.image1);
+        //set a onclick listener for when the button gets clicked
+        aiMl.setOnClickListener(new View.OnClickListener() {
+            //Start new list activity
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(getActivity(), Register_Student_Activity.class);
+                startActivity(mainIntent);
+            }
+        });
+    }*/
 }
