abea5e0a4e307d73e92b814a0fe0165b877c4fb0
aagam109 <aagamshah1090@gmail.com>
2019-10-05 19:56:44 +0530

    Added Quiz_Activity

---
+++ app/src/main/java/com/ankita/studbud/Quiz_Android.java
@@ -0,0 +1,15 @@
+package com.ankita.studbud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class Quiz_Android extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz__android);
+    }
+}
