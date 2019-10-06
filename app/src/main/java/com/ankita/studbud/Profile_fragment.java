0a959aeb51a9de9296cc4e90fbf95004634b856d
AnkitaP <ankita.a.patil123@gmail.com>
2019-10-06 11:12:48 +0530

    Profile DB upload

--- app/src/main/java/com/ankita/studbud/Profile_fragment.java
+++ app/src/main/java/com/ankita/studbud/Profile_fragment.java
@@ -1,6 +1,7 @@
 package com.ankita.studbud;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -8,14 +9,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 
 public class Profile_fragment extends Fragment {
@@ -30,6 +40,8 @@ public class Profile_fragment extends Fragment {
 
     TextView username;
     TextView name;
+    TextView tag1;
+    TextView tag2,tag3,tag4;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -49,8 +61,45 @@ public class Profile_fragment extends Fragment {
         username=view.findViewById(R.id.user_profile_short_bio);
         name=view.findViewById(R.id.user_profile_name);
         //username.setText(mRef.child(mfirebaseAuth.getInstance().getCurrentUser().getUid()+"/Name").getValue());
-        username.setText(mfirebaseAuth.getInstance().getCurrentUser().getEmail());
-        name.setText(mfirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+        //username.setText(mfirebaseAuth.getInstance().getCurrentUser().getEmail());
+        tag1=view.findViewById(R.id.tag1);
+        tag3=view.findViewById(R.id.tag3);
+        tag4=view.findViewById(R.id.tag4);
+        tag2=view.findViewById(R.id.tag2);
+        mRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot snapshots: dataSnapshot.getChildren()) {
+                    try {
+                        Map<Object, String> data = (Map<Object, String>) snapshots.getValue();
+
+                        name.setText(data.get("Name"));
+                        username.setText(data.get("Email id"));
+                        tag1.setText("Score for Quiz one : "+data.get("AND_Course1"));
+                        tag2.setText("Score of Quiz two : "+data.get("AND_Course2"));
+                        tag3.setText("Email : "+data.get("Email id"));
+                        tag4.setText("Username : "+data.get("Name"));
+
+
+                    }
+
+                    catch (Exception e){
+                        Log.i(TAG, "onDataChange: "+e.getMessage());
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
 
     }
 
