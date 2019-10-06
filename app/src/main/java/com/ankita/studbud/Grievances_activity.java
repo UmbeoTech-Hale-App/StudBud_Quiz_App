928f3121e3cb10f6bad798ad35a0083ad36d59ef
AnkitaP <ankita.a.patil123@gmail.com>
2019-10-06 09:32:30 +0530

    UI Updates 7.0

---
+++ app/src/main/java/com/ankita/studbud/Grievances_activity.java
@@ -0,0 +1,14 @@
+package com.ankita.studbud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Grievances_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grievances_activity);
+    }
+}
